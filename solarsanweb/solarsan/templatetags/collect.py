a393abe8183f14b294598bb7903ec768baa49b40
Trevor Joynson (trevorj) <trevorj@localhostsolutions.com>
2012-01-09 17:16:25 -0500

    Add custom templatetags

---
+++ solarsanweb/solarsan/templatetags/collect.py
@@ -0,0 +1,23 @@
+from django import template
+
+register = template.Library()
+
+@register.tag
+def collect(parser, token):
+    bits = list(token.split_contents())
+    if len(bits) > 3 and bits[-2] == 'as':
+        varname = bits[-1]
+        items = bits[1:-2]
+        return CollectNode(items, varname)
+    else:
+        raise template.TemplateSyntaxError('%r expected format is "item [item ...] as varname"' % bits[0])
+
+class CollectNode(template.Node):
+    def __init__(self, items, varname):
+        self.items = map(template.Variable, items)
+        self.varname = varname
+
+    def render(self, context):
+        context[self.varname] = [i.resolve(context) for i in self.items]
+        return ''
+
