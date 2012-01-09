8f9f703cfd3fb8f09f6c72c4107b61787a3350d9
Trevor Joynson (trevorj) <trevorj@localhostsolutions.com>
2012-01-09 17:14:02 -0500

    Use real zpool iostat for live graphs. Need to make some lines with history, but it's a start.

--- solarsanweb/solarsan/views.py
+++ solarsanweb/solarsan/views.py
@@ -18,32 +18,31 @@ def status(request):
 
 @csrf_exempt
 def graph_stats(request):
-    if request.GET.get('graph') == 'graph_utilization':
-        graph = []
-        
-        zfs_dataset = 'rpool'
-        zfs_lists = str(os.popen('/usr/sbin/zfs list -H -d 0 -t filesystem '+zfs_dataset).read()).splitlines()
-        for i in zfs_lists:
-            i = str(i).split()
-            used = int(convert_human_to_bytes(i[1]))
-            free = int(convert_human_to_bytes(i[2]))
-            total = int(used + free)
-            graph.append({'label': i[0]+' used', 'data': float(used / float(total) * 100) })
-            graph.append({'label': i[0]+' free', 'data': float(free / float(total) * 100) })
+    graph = {}
+    
+#    zfs_lists = str(os.popen('/usr/sbin/zfs list -H -d 0 -t filesystem '+zfs_dataset).read()).splitlines()
+#    for i in zfs_lists:
+#        i = str(i).split()
+#        used = int(convert_human_to_bytes(i[1]))
+#        free = int(convert_human_to_bytes(i[2]))
+#        total = int(used + free)
+#        graph['graph_utilization'] = {'values': [float(used / float(total) * 100), float(free / float(total) * 100)] }
 
-    elif request.GET.get('graph') == 'graph_iops':
-        iostats = str(os.popen('/usr/sbin/zpool iostat rpool | tail -n +4').read()).split()
-        graph = [{'label': 'IOPs',
-                  'data': [
-                           ]
-                  }]
-    elif request.GET.get('graph') == 'graph_throughput':
-        graph = [{'label': 'throughput',
-                 'data': [[1,1],
-                          [2,5],
-                          [3,3]]}]
-    else:
-        return
+    zfs_dataset = 'rpool'
+    iostats = str(os.popen('/usr/sbin/zpool iostat \''+zfs_dataset+'\' 2 2 | tail -n +5').read()).split()
+    
+    used = int(convert_human_to_bytes(iostats[1]))
+    free = int(convert_human_to_bytes(iostats[2]))
+    total = int(used + free)
+    graph['graph_utilization'] = {'values': [float(used / float(total) * 100), float(free / float(total) * 100)] }
+    
+    iops_read = int(iostats[3])
+    iops_write = int(iostats[4])
+    graph['graph_iops'] = {'values': [iops_read, iops_write]}
+    
+    read = int(convert_human_to_bytes(iostats[5]))
+    write = int(convert_human_to_bytes(iostats[5]))
+    graph['graph_throughput'] = {'values': [read, write]}
     
     return JSONResponse(graph)
 
