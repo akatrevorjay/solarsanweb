5df216b88b5c964b05dd2223b0d59b22827820ad
Trevor Joynson (trevorj) <trevorj@localhostsolutions.com>
2012-01-16 00:00:08 -0500

    Add zfs_list util, update styling, misc

--- solarsanweb/solarsan/utils.py
+++ solarsanweb/solarsan/utils.py
@@ -4,6 +4,100 @@ from django.utils import simplejson
 from django.utils.encoding import force_unicode
 from django.db.models.base import ModelBase
 
+import string, os, sys
+
+def zfs_list(opts={}):
+    """ Utility to get a list of zfs volumes and associated properties.
+    Also aggregates parent/children information """
+    
+    args = ''
+    
+    if not opts.has_key('type'):
+        opts['type']='filesystem'
+    args += ' -t '+opts['type']
+
+    if opts.has_key('depth'):
+        args += ' -d '+str(opts['depth'])
+
+    if opts.has_key('props'):
+        zfs_props = opts['props']
+    else:
+        zfs_props = ['name', 'type', 'used', 'available', 'creation', 'referenced',
+                     'compressratio', 'mounted', 'quota', 'reservation', 'recordsize',
+                     'mountpoint', 'sharenfs', 'checksum', 'compression', 'atime',
+                     'devices', 'exec', 'setuid', 'readonly', 'zoned', 'snapdir',
+                     'aclinherit', 'canmount', 'xattr', 'copies', 'version', 'utf8only',
+                     'normalization', 'casesensitivity', 'vscan', 'nbmand', 'sharesmb',
+                     'refquota', 'refreservation', 'primarycache', 'secondarycache',
+                     'usedbysnapshots', 'usedbydataset', 'usedbychildren',
+                     'usedbyrefreservation', 'logbias', 'dedup', 'mlslabel', 'sync',
+                     'refcompressratio']
+    args += ' -o '+','.join(zfs_props)
+
+    if opts.has_key('dataset'):
+        args += ' '+opts['dataset']
+
+    datasets = {}
+    
+    zfs_list_out = str(os.popen('/usr/sbin/zfs list -H'+args).read()).splitlines()
+    for i in zfs_list_out:
+        i = str(i).split("\t")
+        d = {}
+
+        for j_count,j in enumerate(zfs_props):
+            d[j] = i[j_count]
+        
+        datasets[d['name']] = d
+
+    for d in datasets:
+        d = datasets[d]
+        
+        (d['parent'], d['basename']) = os.path.split(d['name'])
+
+        if len(d['basename']) <= 0 or len(d['parent']) <= 0:
+            continue
+        
+        if not datasets[d['parent']].has_key('children'):
+            datasets[d['parent']]['children'] = []
+        datasets[d['parent']]['children'].append(d['name'])
+
+    return datasets
+
+def zpool_iostat(zpool, capture_length):
+    """ Utility to return zpool iostat on the specified zpool """
+    return str(os.popen('/usr/sbin/zpool iostat \''+zpool+'\' '+str(capture_length)+' 2 | tail -n +5').read()).split()
+
+
+
+def convert_bytes_to_human(n):
+    """ Utility to convert bytes to human readable (K/M/G/etc) """
+    SYMBOLS = ('K', 'M', 'G', 'T', 'P', 'E', 'Z', 'Y')
+    PREFIX = {}
+    for i, j in enumerate(SYMBOLS):
+        PREFIX[j] = 1 << (i+1)*10
+
+    for i in reversed(SYMBOLS):
+        if n >= PREFIX[i]:
+            value = float(n) / PREFIX[i]
+            return '%.1f%s' % (value, i)
+    
+def convert_human_to_bytes(s):
+    """ Utility to convert human readable (K/M/G/etc) to bytes """
+    SYMBOLS = ('K', 'M', 'G', 'T', 'P', 'E', 'Z', 'Y')
+    PREFIX = {}
+    for i, j in enumerate(SYMBOLS):
+        PREFIX[j] = 1 << (i+1)*10
+
+    s = str(s).upper()
+    for i in SYMBOLS:
+        if s.endswith(i):
+            return '%.0f' % (float(s[:-1]) * PREFIX[i])
+
+    # Must be bytes or invalid data
+    #TODO What if it's invalid data? How should that be handled?
+    return '%.0f' % float(s)
+
+
 class LazyJSONEncoder(simplejson.JSONEncoder):
     """ a JSONEncoder subclass that handle querysets and models objects. Add
     your code about how to handle your type of object here to use when dumping
@@ -47,29 +141,3 @@ def qdct_as_kwargs(qdct):
     for k,v in qdct.items():
         kwargs[str(k)]=v
     return kwargs
-
-def convert_bytes_to_human(n):
-    SYMBOLS = ('K', 'M', 'G', 'T', 'P', 'E', 'Z', 'Y')
-    PREFIX = {}
-    for i, j in enumerate(SYMBOLS):
-        PREFIX[j] = 1 << (i+1)*10
-
-    for i in reversed(SYMBOLS):
-        if n >= PREFIX[i]:
-            value = float(n) / PREFIX[i]
-            return '%.1f%s' % (value, i)
-    
-def convert_human_to_bytes(s):
-    SYMBOLS = ('K', 'M', 'G', 'T', 'P', 'E', 'Z', 'Y')
-    PREFIX = {}
-    for i, j in enumerate(SYMBOLS):
-        PREFIX[j] = 1 << (i+1)*10
-
-    s = str(s).upper()
-    for i in SYMBOLS:
-        if s.endswith(i):
-            return '%.0f' % (float(s[:-1]) * PREFIX[i])
-
-    # Must be bytes or invalid data
-    #TODO What if it's invalid data? How should that be handled?
-    return '%.0f' % float(s)
--- solarsanweb/solarsan/views.py
+++ solarsanweb/solarsan/views.py
@@ -8,34 +8,25 @@ from utils import qdct_as_kwargs
 from response import JSONResponse
 from django.views.decorators.csrf import csrf_exempt
 
-import string, os, sys
-from solarsan.utils import convert_human_to_bytes
+from solarsan.utils import convert_human_to_bytes, zpool_iostat, zfs_list
 
 def status(request):
+    """ Displays status of SAN """
     return render_to_response('solarsan/status.html',
         {'title': 'Status',
-         'graph_stats': graph_stats()},
+         'graph_stats': graph_stats(1)},
         context_instance=RequestContext(request))
 
-def zfs_list():
-    zfs_lists = str(os.popen('/usr/sbin/zfs list -H -d 0 -t filesystem '+zfs_dataset).read()).splitlines()
-    for i in zfs_lists:
-        i = str(i).split()
-        used = int(convert_human_to_bytes(i[1]))
-        free = int(convert_human_to_bytes(i[2]))
-        total = int(used + free)
-        graph['graph_utilization'] = {'values': [float(used / float(total) * 100), float(free / float(total) * 100)] }
+def graph_stats(capture_length):
+    if not capture_length:
+        capture_length = 2
 
-def zpool_iostat(zfs_dataset, capture_length):
-    return str(os.popen('/usr/sbin/zpool iostat \''+zfs_dataset+'\' '+str(capture_length)+' 2 | tail -n +5').read()).split()
-
-def graph_stats():
-    iostats = zpool_iostat('rpool', 2)
+    iostats = zpool_iostat('rpool', capture_length)
     
     graph = {}
 
-    used = int(convert_human_to_bytes(iostats[1]))
-    free = int(convert_human_to_bytes(iostats[2]))
+    used = int(convert_human_to_bytes(iostats[1])) / 1024 / 1024
+    free = int(convert_human_to_bytes(iostats[2])) / 1024 / 1024
     total = int(used + free)
     graph['graph_utilization'] = {'values': [float(used / float(total) * 100), float(free / float(total) * 100)] }
     
@@ -43,13 +34,13 @@ def graph_stats():
     iops_write = int(iostats[4])
     graph['graph_iops'] = {'values': [iops_read, iops_write]}
     
-    read = int(convert_human_to_bytes(iostats[5]))
-    write = int(convert_human_to_bytes(iostats[6]))
+    read = int(convert_human_to_bytes(iostats[5])) / 1024 / 1024
+    write = int(convert_human_to_bytes(iostats[6])) / 1024 / 1024
     graph['graph_throughput'] = {'values': [read, write]}
 
     return graph
 
 @csrf_exempt
 def graph_stats_json(request):
-    return JSONResponse(graph_stats())
+    return JSONResponse(graph_stats(1))
 
