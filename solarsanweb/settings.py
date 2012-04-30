e98a6d62179b81261fdec5366c31365a09654e13
Trevor Joynson (trevorj) <trevorj@localhostsolutions.com>
2012-04-30 02:11:47 -0400

    Use supervisor/supervisord for spawned procs for ease, much better autoreloading this way

--- solarsanweb/settings.py
+++ solarsanweb/settings.py
@@ -28,6 +28,7 @@ DATABASES = {
 # If running in a Windows environment this must be set to the same as your
 # system time zone.
 TIME_ZONE = 'America/New_York'
+USE_TZ = True
 
 # Language code for this installation. All choices can be found here:
 # http://www.i18nguy.com/unicode/language-identifiers.html
@@ -162,6 +163,7 @@ INSTALLED_APPS = (
     'debug_toolbar',
 	'django_extensions',
 	'chartit',
+    'djsupervisor',
 
 	# Apps
     'solarsan',
