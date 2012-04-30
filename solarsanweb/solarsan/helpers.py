cb4362b3cd3373ea95188f988bdf3801d165a61a
Trevor Joynson (trevorj) <trevorj@localhostsolutions.com>
2012-04-30 08:26:13 -0400

    Testing flot again, this time against the generated RRDs

--- solarsanweb/solarsan/helpers.py
+++ solarsanweb/solarsan/helpers.py
@@ -8,3 +8,23 @@ def load_charts(*args):
     """ Writes out django-chartit charts; this is a wrapper around the django templatetag """
     return jinja2.Markup(chartit.templatetags.chartit.load_charts(*args))
 
+@register.filter
+def flotjson(graph, container, **kwargs):
+    """ Writes out JSON for a flot graph"""
+
+    out=[]
+
+    if kwargs.get('wrapper', True):
+        out.append('<script id="source" language="javascript" type="text/javascript">')
+        out.append('$(function () {')
+
+    out.append('$.plot($("#%s"), %s, %s);' %
+               (container, graph.series_json, graph.options_json) )
+
+
+    if kwargs.get('wrapper', True):
+        out.append('});')
+        out.append('</script>')
+
+    return jinja2.Markup("\n".join(out))
+
--- solarsanweb/solarsan/views.py
+++ solarsanweb/solarsan/views.py
@@ -7,12 +7,93 @@ from django.views.decorators.csrf import csrf_exempt
 from solarsan.utils import *
 from solarsan.models import *
 
+from datetime import datetime, timedelta
+from time import time
+from pyrrd.rrd import RRD
+from pyrrd.backend import bindings
+
+from django.conf import settings
+import pyflot
+import os, sys
+
 def status(request):
 
     pools = Pool.objects.all()
 
+    graphs = {}
+    for rrd_file in ['cache_result', 'arc_hitmiss', 'cache_size', 'cache_eviction']:
+        #graph.add_line([ (x,y), (x,y) ])
+        rrd_path=os.path.join(settings.DATA_DIR, 'rrd', rrd_file + '.rrd')
+        rrd = RRD(rrd_path, mode="r")
+
+        endTime = datetime.now()
+        startTime = endTime - timedelta(hours=5)
+        
+        graphs[rrd_file] = pyflot.Flot()
+        rrd = RRD(rrd_path, mode="r")
+        rrd_data = rrd.fetch(resolution=300 * 12, cf='AVERAGE',
+                  start=startTime.strftime('%s'),
+                  end=endTime.strftime('%s'),
+                  returnStyle='ds')
+
+        for ds in rrd_data.keys():
+            d = []
+            for (x,y) in rrd_data[ds]:
+                try:
+                    time = int(datetime.fromtimestamp(int(x)).strftime('%s')) * 1000
+                    data = int(y)
+                    if data > 1000:
+                        data = data / 1000 / 1000
+                except:
+                    data = 0
+                d.append( ( time, data ) )
+            graphs[rrd_file].add_series(d, label=ds, )
+
+        graphs[rrd_file]._options = {
+            'xaxis': {  'mode': 'time',
+                        #'position': 'top',
+                        #'tickDecimals': 0, 'tickSize': 1,
+                        'minTickSize': [1, "minute"],
+                        'twelveHourClock': True,
+                        'timeFormat': '%I:%m%p',
+                        #'autoscaleMargin': 1,
+                        #'reserveSpace': False,
+                        },
+            'yaxis': {  #'autoscaleMargin': 1,
+                        #'reserveSpace': False,
+                        'labelWidth': 25,
+                     },
+            'lines': { 'show': True, },
+            'points': { 'show': True, },
+            'height': '200px',
+            'grid': {
+                'backgroundColor': { 'colors': ["#fff", "#eee"] },
+                #'hoverable': True,
+                #'autoHighlight': True,
+                #'axisMargin': 0,
+                #'labelMargin': 0,
+                #'': ,
+                },
+            #'crosshair': { 'mode': "x" },
+            'legend': {
+                'show': True,
+                #labelFormatter: null or (fn: string, series object -> string)
+                #labelBoxBorderColor: color
+                'noColumns': 2,
+                'position': "nw",
+                #position: "ne" or "nw" or "se" or "sw"
+                #margin: number of pixels or [x margin, y margin]
+                #'margin': 0,
+                #backgroundColor: null or color
+                #backgroundOpacity: number between 0 and 1
+                'backgroundOpacity': 0.75,
+                #container: null or jQuery object/DOM element/jQuery expression
+                #'container': '#flotconttest',
+            },
+        }
+
     def convert_timestamp(dt):
-        return dt.strftime('%H:%M%P')
+        return dt.strftime('%I:%M%P')
 
    ## TODO ArcStats
     ds = DataPool(
@@ -105,7 +186,8 @@ def status(request):
         {'title': 'Status',
          'pools': pools,
          'graph_capacity': graph_capacity,
-         'graph_activity': graph_activity, },
+         'graph_activity': graph_activity,
+         'graphs': graphs, },
         context_instance=RequestContext(request))
 
 @csrf_exempt
