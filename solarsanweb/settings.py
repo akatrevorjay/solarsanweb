4e8dc009c43e5d70ec1141c1d0d8f720368e3d0a
Trevor Joynson (trevorj) <trevorj@localhostsolutions.com>
2012-05-01 22:11:15 -0400

    Use new relative URLs by default for development

--- solarsanweb/settings.py
+++ solarsanweb/settings.py
@@ -66,7 +66,7 @@ MEDIA_ROOT = os.path.join(TOP_DIR, "media")
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
 # Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
-MEDIA_URL = ''
+MEDIA_URL = '/solarsan/media/'
 
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
@@ -76,7 +76,7 @@ STATIC_ROOT = os.path.join(TOP_DIR, "static")
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
-STATIC_URL = '/static/'
+STATIC_URL = '/solarsan/static/'
 
 # URL prefix for admin static files -- CSS, JavaScript and images.
 # Make sure to use a trailing slash.
