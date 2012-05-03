5577239b2ec24db4f9d7502eb7b749cb2b40a550
Trevor Joynson (trevorj) <trevorj@localhostsolutions.com>
2012-05-03 06:27:54 -0400

    Put static/media dirs back to normal

--- solarsanweb/settings.py
+++ solarsanweb/settings.py
@@ -66,7 +66,7 @@ MEDIA_ROOT = os.path.join(TOP_DIR, "media")
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
 # Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
-MEDIA_URL = '/solarsan/media/'
+MEDIA_URL = '/media/'
 
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
@@ -76,12 +76,12 @@ STATIC_ROOT = os.path.join(TOP_DIR, "static")
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
-STATIC_URL = '/solarsan/static/'
+STATIC_URL = '/static/'
 
 # URL prefix for admin static files -- CSS, JavaScript and images.
 # Make sure to use a trailing slash.
 # Examples: "http://foo.com/static/admin/", "/static/admin/".
-ADMIN_MEDIA_PREFIX = '/solarsan/static/admin/'
+#ADMIN_MEDIA_PREFIX = '/static/admin/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
