8f3416012f897e54b94a233cfcb8a1634850d588
Trevor Joynson (trevorj) <trevorj@localhostsolutions.com>
2012-05-09 16:00:03 -0400

    Added some model ZFS functions

--- solarsanweb/solarsan/models.py
+++ solarsanweb/solarsan/models.py
@@ -52,6 +52,46 @@ class Pool(models.Model):
         """ Returns the matching Dataset for Pool """
         return self.dataset_set.get(name=self.name)
 
+    def filesystem_create(self, name, **kwargs):
+        """ Creates a filesystem in the pool """
+        logging.info('Request to create filesystem (%s) on pool (%s)', name, self.name)
+        # Get DB entry ready (and validate data)
+        filesystem = Filesystem(name=name, pool_id=self.id)
+        filesystem.save()
+        # Return saved filesystem object
+        return filesystem
+
+    @property
+    def zfs(self):
+        """ Returns ZFS object for this dataset """
+        zpool_list = zfs.zpool_list(self.name)
+        return zpool_list[self.name]
+
+    def delete(self, *args, **kwargs):
+        """ Overridden delete that deletes the underlying ZFS object before deleting from DB """
+        try:
+            # Does the object exist in ZFS?
+            zfs_pool = self.zfs
+            zfs_existing = True
+        except(Exception):
+            logging.error("Was asked to delete ZFS pool '%s', but this pool does not exist in ZFS. "
+                    + "Deleting from DB as it's not real anyway.", self.name)
+            zfs_existing = False
+        # If so, can we destroy it?
+        if zfs_existing == True:
+            try:
+                #zfs.zfs_destroy(self.name)
+                logging.error("DEBUG: NOT DELETING REGARDLESS OF WHAT YOU TELL ME")
+            except:
+                raise Exception("Failed to delete existing ZFS %s (%s), not deleting from DB",
+                        zfs_pool['type'], self.name)
+        # Delete from DB
+        super(Pool, self).delete(*args, **kwargs)
+
+    def iostat(self, **kwargs):
+        """ Returns newly generated ZFS IOStats """
+        zpool_iostat = zfs.zpool_iostat(self.name, **kwargs)
+        return zpool_iostat[self.name]
 
 class Pool_IOStat(models.Model):
     pool = models.ForeignKey(Pool)
@@ -148,25 +188,22 @@ class Dataset(models.Model):
     def delete(self, *args, **kwargs):
         """ Overridden delete that deletes the underlying ZFS object before deleting from DB """
         try:
-            from_db_only = kwargs.pop('from_db_only')
-        except (KeyError):
-            from_db_only = False
-
-        if from_db_only == False:
+            # Does the object exist in ZFS?
+            zfs_dataset = self.zfs
+            zfs_existing = True
+        except(Exception):
+            logging.error("Was asked to delete ZFS dataset '%s', but this dataset does not exist in ZFS. "
+                    + "Deleting from DB as it's not real anyway.", self.name)
+            zfs_existing = False
+        # If so, can we destroy it?
+        if zfs_existing == True:
             try:
-                # Does the object exist in ZFS?
-                if self.zfs:
-                    # Yes? Can we destroy it?
-                    try:
-                        zfs.zfs_destroy(self.name)
-                    except:
-                        raise Exception("Failed to delete existing ZFS dataset '%s', not deleting from DB",
-                                self.name)
-            except(Exception):
-                logging.error("Was asked to delete ZFS dataset '%s', but this dataset does not exist in ZFS. "
-                        + "Deleting from DB as it's not real anyway.", self.name)
-                pass
-        # Delete from DB
+                #zfs.zfs_destroy(self.name)
+                logging.error("DEBUG: NOT DELETING REGARDLESS OF WHAT YOU TELL ME")
+            except:
+                raise Exception("Failed to delete existing ZFS %s (%s), not deleting from DB",
+                        self.type, self.name)
+       # Delete from DB
         super(Dataset, self).delete(*args, **kwargs)
 
 
@@ -190,6 +227,36 @@ class Filesystem(Dataset):
         snapshot.save()
         # Return saved snapshot object
         return snapshot
+
+    def save(self):
+        """ Creates a filesystem and saves it to the DB """
+        # Data validation should probably be done in zfs.zfs_create()
+        # TODO Isn't there a transactional commit decorator?
+        # Does filesystem exist already on filesystem?
+        name = self.name
+        try:
+            # If it already exists, allow it through.
+            zfs_ret = zfs.zfs_list(name)
+            zfs_existing = True
+            logging.info("While creating ZFS filesystem (%s), we've noticed it already exists in ZFS. "
+                    + "Leaving filesystem as is, adding it to DB", name)
+        except:
+            # Good, it doesn't exist. Create it.
+            zfs_ret = zfs.zfs_create(name)
+            zfs_existing = False
+            logging.info("Created ZFS filesystem (%s)", name)
+        # Normalize data and stuff it into thyself
+        try:
+            for key,val in zfs_ret[name].iteritems():
+                setattr(self, key, val)
+            # Cool, now save to DB and hand it back
+            super(Filesystem, self).save(self)
+        except:
+            if zfs_existing == False:
+                zfs.zfs_destroy(self.name)
+            raise Exception("Created ZFS filesystem but it could not be saved to DB. "
+                    + "Destroying ZFS filesystem if it was created by this task")
+
 
 
 
@@ -216,8 +283,8 @@ class Snapshot(Dataset):
         """ Returns the associated filesystem for this snapshot """
         return Filesystem.objects.get(name=self.filesystem_name)
 
-    def save(self):
-        """ Creates a filesystem snapshot and saves it to the DB (static) """
+    def save(self, **kwargs):
+        """ Creates a filesystem snapshot and saves it to the DB """
         # Data validation should probably be done in zfs.zfs_snapshot()
         # TODO Isn't there a transactional commit decorator?
         # Does snapshot exist already on filesystem?
@@ -225,9 +292,13 @@ class Snapshot(Dataset):
         try:
             # If it already exists, allow it through.
             zfs_ret = zfs.zfs_list(name)
+            zfs_existing = True
+            logging.info("While creating ZFS snapshot (%s), we've noticed it already exists in ZFS. "
+                    + "Leaving filesystem as is, adding it to DB", name)
         except:
             # Good, it doesn't exist. Create it.
             zfs_ret = zfs.zfs_snapshot(name)
+            zfs_existing = False
         # Normalize data and stuff it into thyself
         try:
             for key,val in zfs_ret[name].iteritems():
@@ -235,8 +306,10 @@ class Snapshot(Dataset):
             # Cool, now save to DB and hand it back
             super(Snapshot, self).save(self)
         except:
-            zfs.zfs_destroy(self.name)
-            raise Exception("Created ZFS snapshot but it could not be saved to DB; Destroyed snapshot")
+            if zfs_existing == False:
+                zfs.zfs_destroy(self.name)
+            raise Exception("Created ZFS snapshot but it could not be saved to DB. "
+                    + "Destroying ZFS snapshot if it was created by this task")
 
 #class Snapshot_Backup_Log(models.Model):
 #    dataset = models.ForeignKey(Dataset)
