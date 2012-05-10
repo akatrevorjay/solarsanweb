0c9dac24cdbd8a2372f0d590ffae02714c3a2897
Trevor Joynson (trevorj) <trevorj@localhostsolutions.com>
2012-05-10 04:45:38 -0400

    Comment out unused and obsolete stuff

--- solarsanweb/solarsan/helpers.py
+++ solarsanweb/solarsan/helpers.py
@@ -1,12 +1,12 @@
 import jinja2
 
 from jingo import register
-import chartit.templatetags.chartit
+#import chartit.templatetags.chartit
 
-@register.filter
-def load_charts(*args):
-    """ Writes out django-chartit charts; this is a wrapper around the django templatetag """
-    return jinja2.Markup(chartit.templatetags.chartit.load_charts(*args))
+#@register.filter
+#def load_charts(*args):
+#    """ Writes out django-chartit charts; this is a wrapper around the django templatetag """
+#    return jinja2.Markup(chartit.templatetags.chartit.load_charts(*args))
 
 @register.filter
 def flotjson(graph, container, **kwargs):
