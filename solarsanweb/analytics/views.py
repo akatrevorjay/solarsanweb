2686c366896dcaa2443b029e6732222d9a3e9a11
Trevor Joynson (trevorj) <trevorj@localhostsolutions.com>
2012-06-15 16:44:48 -0400

    Update, use new templated details view

--- solarsanweb/analytics/views.py
+++ solarsanweb/analytics/views.py
@@ -30,11 +30,84 @@ def home(request, *args, **kwargs):
         context_instance=RequestContext(request))
 
 
-@cache_page(15)
+#@cache_page(15)
 def graphs(request, *args, **kwargs):
-    pools = Pool.objects.all()
-
+    pool = Pool.objects.all()[0]
     graphs = {}
+
+    for iostat_graph in ['bandwidth', 'iops']: #usage
+        graphs[iostat_graph] = pyflot.Flot()
+
+    count = 50
+    iostats = pool.pool_iostat_set.order_by('timestamp')[:count]
+
+    #total = int(iostats[0].alloc + iostats[0].free)
+    #graph['usage'] = {'values': [float(iostats[0].alloc / float(total) * 100), float(iostats[0].free / float(total) * 100)] }
+
+    iops = {'read': [], 'write': []}
+    bandwidth = {'read': [], 'write': []}
+
+    for iostat in iostats:
+        time = int(iostat.timestamp.strftime('%s')) * 1000
+
+        iops['read'].append( (time, int(iostat.iops_read) ) )
+        iops['write'].append( (time, int(iostat.iops_write) ) )
+        bandwidth['read'].append( (time, int(iostat.bandwidth_read) ) )
+        bandwidth['write'].append( (time, int(iostat.bandwidth_write) ) )
+
+    graphs['iops'].add_series(iops['read'], label='IOPs Read', )
+    graphs['iops'].add_series(iops['write'], label='IOPs Write', )
+
+    graphs['bandwidth'].add_series(bandwidth['read'], label='BW Read', )
+    graphs['bandwidth'].add_series(bandwidth['write'], label='BW Write', )
+
+    for iostat_graph in ['bandwidth', 'iops']: #usage
+        graphs[iostat_graph]._options = {
+            'xaxis': {  'mode': 'time',
+                        #'position': 'top',
+                        #'tickDecimals': 0, 'tickSize': 1,
+                        'minTickSize': [1, "minute"],
+                        'twelveHourClock': True,
+                        'timeFormat': '%I:%m%p',
+                        #'autoscaleMargin': 1,
+                        #'reserveSpace': False,
+                        #'localTimezone': True,
+                        },
+            'yaxis': {  #'autoscaleMargin': 1,
+                        #'reserveSpace': False,
+                        'labelWidth': 25,
+                     },
+            'lines': { 'show': True, },
+            'points': { 'show': True, },
+            'height': '200px',
+            'grid': {
+                'backgroundColor': { 'colors': ["#fff", "#eee"] },
+                #'hoverable': True,
+                #'autoHighlight': True,
+                #'axisMargin': 0,
+                #'labelMargin': 0,
+                #'': ,
+                },
+            #'crosshair': { 'mode': "x" },
+            'legend': {
+                'show': True,
+                #labelFormatter: null or (fn: string, series object -> string)
+                #labelBoxBorderColor: color
+                'noColumns': 2,
+                'position': "ne",
+                #position: "ne" or "nw" or "se" or "sw"
+                #margin: number of pixels or [x margin, y margin]
+                #'margin': 0,
+                #backgroundColor: null or color
+                #backgroundOpacity: number between 0 and 1
+                'backgroundOpacity': 0.75,
+                #container: null or jQuery object/DOM element/jQuery expression
+                #'container': '#flotconttest',
+            },
+        }
+
+
+
     for rrd_file in ['cache_result', 'arc_hitmiss', 'cache_size', 'dmu_tx']:
         #graph.add_line([ (x,y), (x,y) ])
         rrd_path=os.path.join(settings.DATA_DIR, 'rrd', rrd_file + '.rrd')
@@ -108,7 +181,7 @@ def graphs(request, *args, **kwargs):
         }
 
     return render_to_response('analytics/graphs.html',
-            {'graphs': graphs, 'pools': pools,
+            {'graphs': graphs,
                 }, context_instance=RequestContext(request))
 
 
--- solarsanweb/solarsan/helpers.py
+++ solarsanweb/solarsan/helpers.py
@@ -1,12 +1,5 @@
 import jinja2
-
 from jingo import register
-#import chartit.templatetags.chartit
-
-#@register.filter
-#def load_charts(*args):
-#    """ Writes out django-chartit charts; this is a wrapper around the django templatetag """
-#    return jinja2.Markup(chartit.templatetags.chartit.load_charts(*args))
 
 @register.filter
 def flotjson(graph, container, **kwargs):
