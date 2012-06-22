bad8b1a270aeebbebc0c3bf6da9f94347a9f2d90
Trevor Joynson (trevorj) <trevorj@localhostsolutions.com>
2012-06-22 03:24:15 -0400

    Fix storage urls 2

--- solarsanweb/storage/urls.py
+++ solarsanweb/storage/urls.py
@@ -2,16 +2,16 @@
 from django.conf.urls.defaults import patterns, url, include
 from django.views import generic
 
-import pools.views
-from solarsan.models import Pool
+import storage.views
+#from solarsan.models import Pool, Dataset, Snapshot
 
-urlpatterns = patterns('pools.views',
-    (r'^pools/health/(?P<slug>[A-z0-9_\-]+)$', pools.views.PoolHealthDetailView.as_view()),
-    (r'^pools/analytics/(?P<slug>[A-z0-9_\-]+)$', pools.views.PoolAnalyticsDetailView.as_view()),
-    (r'^pools/detail/(?P<slug>[A-z0-9_\-]+)$', pools.views.PoolDetailView.as_view()),
+urlpatterns = patterns('storage.views',
+    (r'^storage.health/(?P<slug>[A-z0-9_\-]+)$', storage.views.PoolHealthDetailView.as_view()),
+    (r'^storage.analytics/(?P<slug>[A-z0-9_\-]+)$', storage.views.PoolAnalyticsDetailView.as_view()),
+    (r'^storage.detail/(?P<slug>[A-z0-9_\-]+)$', storage.views.PoolDetailView.as_view()),
     
-    (r'^datasets/detail/(?P<slug>[A-z0-9_\-/]+)$', pools.views.DatasetDetailView.as_view()),
-    (r'^datasets/health/(?P<slug>[A-z0-9_\-/]+)$', pools.views.DatasetHealthDetailView.as_view()),
-    (r'^datasets/snapshots/(?P<slug>[A-z0-9_\-/]+)$', pools.views.DatasetSnapshotsView.as_view()),
+    (r'^datasets/detail/(?P<slug>[A-z0-9_\-/]+)$', storage.views.DatasetDetailView.as_view()),
+    (r'^datasets/health/(?P<slug>[A-z0-9_\-/]+)$', storage.views.DatasetHealthDetailView.as_view()),
+    (r'^datasets/snapshots/(?P<slug>[A-z0-9_\-/]+)$', storage.views.DatasetSnapshotsView.as_view()),
 )
 
