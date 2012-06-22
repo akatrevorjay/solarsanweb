94720b54efd5b3eab87b4173e1754f9f5656a01a
Trevor Joynson (trevorj) <trevorj@localhostsolutions.com>
2012-06-21 20:47:57 -0400

    Move cluster into configure

--- solarsanweb/configure/views.py
+++ solarsanweb/configure/views.py
@@ -7,7 +7,36 @@ from django.views.decorators.cache import cache_page
 from django import http
 from django.views import generic
 
-#from solarsan.models import Pool, Dataset, Filesystem, Snapshot
+from solarsan.models import Pool, Dataset, Filesystem, Snapshot
+import gluster
+
+class PeerListView(generic.TemplateView):
+    template_name = 'configure/cluster/peer_list.html'
+    def get(self, request, *args, **kwargs):
+        peers = gluster.peer.status()
+        context = {
+                'peers': peers['host'],
+                'peer_count': peers['peers'],
+                }
+        return self.render_to_response(context)
+
+class PeerDetailView(generic.TemplateView):
+    template_name = 'configure/cluster/peer_detail.html'
+    def get(self, request, *args, **kwargs):
+        peers = gluster.peer.status()
+
+        peer_host = kwargs.get('peer')
+        if not peer_host in peers['host'].keys():
+            raise http.Http404
+
+        peer = {peer_host: peers['host'][peer_host]}
+
+        context = {
+                'peers': peers['host'],
+                'peer': peer,
+                'peer_count': peers['peers'],
+               }
+        return self.render_to_response(context)
 
 class HomeListView(generic.TemplateView):
     template_name = 'configure/home_list.html'
