91ef44125cc54528125fdf07de00817ae7733427
Trevor Joynson (trevorj) <trevorj@localhostsolutions.com>
2012-06-22 03:22:11 -0400

    Fix storage urls

--- solarsanweb/storage/urls.py
+++ solarsanweb/storage/urls.py
@@ -6,12 +6,12 @@ import pools.views
 from solarsan.models import Pool
 
 urlpatterns = patterns('pools.views',
-    (r'^health/(?P<slug>[A-z0-9_\-/]+)$', datasets.views.DatasetHealthDetailView.as_view()),
-    (r'^health/(?P<slug>[A-z0-9_\-]+)$', pools.views.PoolHealthDetailView.as_view()),
-    (r'^analytics/(?P<slug>[A-z0-9_\-]+)$', pools.views.PoolAnalyticsDetailView.as_view()),
-
-    (r'^detail/(?P<slug>[A-z0-9_\-]+)$', pools.views.PoolDetailView.as_view()),
-    (r'^dataset/detail/(?P<slug>[A-z0-9_\-/]+)$', pools.views.DatasetDetailView.as_view()),
-    (r'^dataset/snapshots/(?P<slug>[A-z0-9_\-/]+)$', pools.views.DatasetSnapshotsView.as_view()),
+    (r'^pools/health/(?P<slug>[A-z0-9_\-]+)$', pools.views.PoolHealthDetailView.as_view()),
+    (r'^pools/analytics/(?P<slug>[A-z0-9_\-]+)$', pools.views.PoolAnalyticsDetailView.as_view()),
+    (r'^pools/detail/(?P<slug>[A-z0-9_\-]+)$', pools.views.PoolDetailView.as_view()),
+    
+    (r'^datasets/detail/(?P<slug>[A-z0-9_\-/]+)$', pools.views.DatasetDetailView.as_view()),
+    (r'^datasets/health/(?P<slug>[A-z0-9_\-/]+)$', pools.views.DatasetHealthDetailView.as_view()),
+    (r'^datasets/snapshots/(?P<slug>[A-z0-9_\-/]+)$', pools.views.DatasetSnapshotsView.as_view()),
 )
 
