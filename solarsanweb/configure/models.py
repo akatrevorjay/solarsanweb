00f9e80a0e7929da4f18c987bdef5965120844e7
Trevor Joynson (trevorj) <trevorj@localhostsolutions.com>
2012-06-24 19:12:54 -0400

    Add in new cluster settings, begin to work on db for cluster discovery and such.

--- solarsanweb/configure/models.py
+++ solarsanweb/configure/models.py
@@ -1,8 +1,25 @@
+from solarsan.models import EnabledModelManager
+from jsonfield import JSONField
 from django.db import models
+import logging
 
-#class Config(models.Model):
-#    key = models.CharField(max_length=255)
-#    value = models.CharField(max_length=255)
-#    def __unicode__(self):
-#        return self.key
+
+class Config( models.Model ):
+    """ Simple Key<=>JSON store for config entries """
+    key = models.CharField( max_length=255 )
+    value = JSONField()
+    def __unicode__( self ):
+        return self.key + '=' + self.value
+
+
+class ClusterNode( models.Model ):
+    """ Recently seen peers """
+    ip = models.IPAddressField()
+    #enabled = models.BooleanField( default=False )
+    last_seen = models.DateTimeField( auto_now=True )
+    #first_seen = models.DateTimeField()
+    #objects = EnabledModelManager()
+    #objects_all = models.Manager()
+
+
 
--- solarsanweb/configure/tasks.py
+++ solarsanweb/configure/tasks.py
@@ -1,36 +1,56 @@
-from celery.task.base import Task
-import logging
+from celery.schedules import crontab
+from celery.task import periodic_task, task, chord
+from celery.task.base import PeriodicTask, Task
+from celery.task.sets import subtask
+from storage.models import Pool, Pool_IOStat, Dataset, Filesystem, Volume, Snapshot
+from solarsan.utils import convert_bytes_to_human, convert_human_to_bytes
+import datetime, time, logging
+from datetime import timedelta
 from django.utils import timezone
+#from django.core.files.storage import Storage
+from django.core.exceptions import ObjectDoesNotExist
+#from django import db
+#from django.db import import autocommit, commit, commit_manually, commit_on_success, is_dirty, is_managed, rollback, savepoint, set_clean, set_dirty
+from django.conf import settings
 
+
+"""
+Cluster Discovery / Beacon
+"""
+
+from configure.models import ClusterNode
 import beacon
 
-## TODO Make key configurable
-service = {
-    'port': 1787, # 1337 * 1.337
-    'key': 'solarsan-key0', # Cluster Key
-}
+##
+## TODO RESTful service perhaps ysing piston to get information from one SAN to the other after discovery is completed.
+##
 
-class ClusterProber( Task ):
+class Cluster_Discovery( PeriodicTask ):
     """ Probes for new cluster nodes """
+    run_every = timedelta( seconds=settings.SOLARSAN_CLUSTER['discovery'] )
     def run( self, *args, **kwargs ):
         logger = self.get_logger( **kwargs )
-        logger.info( "Probing for new cluster nodes (port=%s,key=%s)", service['port'], service['key'] )
+        logger.info( "Cluster discovery; Probing for new cluster nodes (port=%s,key=%s)",
+                     settings.SOLARSAN_CLUSTER['port'], settings.SOLARSAN_CLUSTER['key'] )
 
-        return beacon.find_all_servers( service['port'], service['key'] )
+        servers = beacon.find_all_servers( settings.SOLARSAN_CLUSTER['port'], settings.SOLARSAN_CLUSTER['key'] )
+        logger.debug( "Cluster discovery; Found: %s", servers )
+        return servers
 
-class ClusterBeacon( Task ):
+
+## FUCK Make this start on startup, spawns it's own thread, no need to pay attention to it after it's started
+class Cluster_Beacon( Task ):
     """ Controls cluster beacon service """
-    b = beacon.Beacon( service['port'], service['key'] )
+    b = beacon.Beacon( settings.SOLARSAN_CLUSTER['port'], settings.SOLARSAN_CLUSTER['key'] )
     def run( self, *args, **kwargs ):
         logger = self.get_logger( **kwargs )
+        logger.debug( "Starting Cluster Beacon.." )
 
-        action = args[0]
+        self.b.daemon = True
+        self.b.start()
 
-        logger.info( "Performing action (%s) on cluster beacon service", action )
+        logger.info( "Started Cluster Beacon." )
 
-        if action == 'start':
-            self.b.daemon = True
-            self.b.start()
-        if action == 'stop':
-            return Exception( 'Not Implemented' )
+
+
 
