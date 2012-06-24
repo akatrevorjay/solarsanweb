5dd71d5e327d51f9a051561bd27b98210356dcb9
Trevor Joynson (trevorj) <trevorj@localhostsolutions.com>
2012-06-23 20:10:13 -0400

    Remove scheduler

--- solarsanweb/scheduler/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render_to_response
-from django.template import RequestContext
-
-def home(request, *args, **kwargs):
-    return render_to_response('scheduler/home.html',
-        {'title': 'Scheduler',
-            },
-        context_instance=RequestContext(request))
-
