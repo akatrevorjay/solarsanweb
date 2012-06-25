0b4727decb515609360aad37388a03115692f47a
Trevor Joynson (trevorj) <trevorj@localhostsolutions.com>
2012-06-24 23:45:43 -0400

    I fucking give up for now, I'm reverting back to what worked 24 hours ago. Lol.

--- solarsanweb/storage/tasks.py
+++ solarsanweb/storage/tasks.py
@@ -2,15 +2,12 @@ from celery.schedules import crontab
 from celery.task import periodic_task, task, chord
 from celery.task.base import PeriodicTask, Task
 from celery.task.sets import subtask
-from storage.models import Pool, Pool_IOStat, Dataset, Filesystem, Volume, Snapshot
+from storage.models import Pool, Pool_IOStat, Dataset, Filesystem, Snapshot
 from solarsan.utils import convert_bytes_to_human, convert_human_to_bytes
 import datetime, time, logging
 from datetime import timedelta
 from django.utils import timezone
 #from django.core.files.storage import Storage
-from django.core.exceptions import ObjectDoesNotExist
-#from django import db
-#from django.db import import autocommit, commit, commit_manually, commit_on_success, is_dirty, is_managed, rollback, savepoint, set_clean, set_dirty
 
 """
 Import pool/dataset info from system into DB
@@ -22,51 +19,88 @@ import zfs
 class Import_ZFS_Metadata( PeriodicTask ):
     """ Syncs ZFS pools/datasets to DB """
     run_every = timedelta( minutes=5 )
+    data = {}
+
     def run( self, *args, **kwargs ):
         logging = self.get_logger()
-        self.data = {'pools': {}, 'filesystems': {}, 'volumes': {}, 'snapshots': {}}
+        self.data = {'pools': {}, 'datasets': {}}
         self.do_level( zfs.tree() )
 
-        # Disable objects which are no longer seen as in existence
-        for ztype_obj in [Pool, Filesystem, Volume, Snapshot]:
-            if ztype_obj._zfs_type + 's' not in self.data.keys(): continue
-            objs = ztype_obj.objects.exclude( name__in=self.data[ztype_obj._zfs_type + 's'].keys() )
-            if objs.count() > 0:
-                logging.info( "Cannot find %s %s on storage; marking disabled: %s", objs.count(), ztype_obj._zfs_type + 's', objs.values_list( 'name' ) )
-                objs.update( enabled=False )
-
-        # Destroy old data array
-        self.data = None
+        # Delete things which are no longer in existence
+        for key, val in {'pools': Pool, 'datasets': Dataset}.items():
+            # Do filter disabled ones from here!
+            for obj in val.objects.all():
+                if not obj.name in list( self.data[key] ):
+                    logging.error( "Cannot find %s '%s' in ZFS, yet is in the DB. Disabling",
+                            obj._zfs_type, obj.name )
+                    obj.enabled = False
+                    obj.save()
+            self.data[key] = []
 
     def do_level( self, cur ):
         """ This gets called for each level of the tree to loop through it, then it's children """
         # First, take care of any objects
-        if getattr( cur, 'has', None ):     self.do_level_items( cur )
+        if getattr( cur, 'has', None ):       self.do_level_items( cur )
+
         # Call this function recursively for each cur[key]
-        for child_key in cur.keys():        self.do_level( cur[child_key] )
+        for child_key in cur.keys():    self.do_level( cur[child_key] )
 
     def do_level_items( self, cur ):
         """ This gets called for each level of the tree to handle the level's items """
         has = getattr( cur, 'has', [] )
-        for ztype_obj in [Pool, Filesystem, Volume, Snapshot]:
-            logging.debug('has=%s cur=%s ztype=%s', has, cur, ztype_obj._zfs_type)
-            if not ztype_obj._zfs_type + 's' in has: continue
-            ztype = ztype_obj._zfs_type
-            for val in getattr( cur, ztype + 's' ).values():
-                if ztype in ['pool']:   del val['type']
-                try:
-                    # The 'name' key of an object is always the full path name; ie, not relative to the tree.
-                    val['enabled'] = True
-                    obj = ztype_obj.objects_unfiltered.filter( name=val['name'] ).update( **val )
-                except ( ObjectDoesNotExist ):
-                    logging.error( 'Found %s "%s".', ztype, val['name'] )
-                    # If we're a kind of dataset, we'll need a pool_id parameter
-                    if ztype in ['filesystem', 'snapshot', 'volume']:
-                        val['pool_id'] = Pool.objects_unfiltered.get( name=val['name'].split( '/' )[0].split( '@' )[0] ).id
-                    obj = ztype_obj( **val )
-                    obj.save( db_only=True )
-                self.data[ztype + 's'][val['name']] = True
 
+        # Pools
+        if 'pools' in has:
+            pools = cur.pools
+            for pv in pools.itervalues():
+                # The 'name' key of an object is always the full path name; ie, not relative to the tree.
+                p = pv['name']
+                try:
+                    pool = Pool.objects_unfiltered.get( name=p )
+                    pv['enabled'] = True
+                    for k, v in pv.iteritems(): setattr( pool, k, v )
+                except ( KeyError, Pool.DoesNotExist ):
+                    logging.error( 'Found pool "%s"', p )
+                    del pv['type']
+                    pool = Pool( **pv )
+                pool.save( db_only=True )
+                self.data['pools'][p] = True
+
+        # Datasets
+        datasets = []
+        if 'filesystems' in has: datasets.extend( cur.filesystems.items() )
+        if 'snapshots' in has:   datasets.extend( cur.snapshots.items() )
+        datasets = dict( datasets )
+        for dv in datasets.values():
+            # The 'name' key of an object is always the full path name; ie, not relative to the tree.
+            d = dv['name']
+            try:
+                # Get existing dataset, if it doesn't exist the except below will catch it
+                if dv['type'] == 'snapshot':        dataset = Snapshot.objects_unfiltered.get( name=d )
+                elif dv['type'] == 'filesystem':    dataset = Filesystem.objects_unfiltered.get( name=d )
+                elif dv['type'] == 'pool':          dataset = Pool.objects_unfiltered.get( name=d )
+                else:
+                    logging.error( "Found unknown type '%s': '%s'", dv['type'], d )
+                    continue
+                # Ensure enabled
+                dv['enabled'] = True
+                # Apply data to existing dataset
+                for k, v in dv.iteritems(): setattr( dataset, k, v )
+            except ( KeyError, Pool.DoesNotExist, Filesystem.DoesNotExist, Snapshot.DoesNotExist ):
+                logging.info( 'Found %s "%s"', dv['type'], d )
+                if dv['type'] == 'pool':
+                    dataset = Pool( **dv )
+                else:
+                    dataset_path = d.split( '/' )
+                    dataset_pool = Pool.objects_unfiltered.get( name=dataset_path[0].split( '@' )[0] )
+                    dv['pool_id'] = dataset_pool.id
+                    if dv['type'] == 'snapshot':
+                        dataset = Snapshot( **dv )
+                    elif dv['type'] == 'filesystem':
+                        dataset = Filesystem( **dv )
+            # Save dataset
+            dataset.save( db_only=True )
+            self.data['datasets'][d] = True
 
 
 class Cleanup_Disabled_Storage_Items( PeriodicTask ):
@@ -76,9 +110,7 @@ class Cleanup_Disabled_Storage_Items( PeriodicTask ):
         logging = self.get_logger()
         # Delete disabled items that are too old to care about.
         for key, val in {'pools': Pool, 'datasets': Dataset}.items():
-            objs = val.objects_unfiltered.filter( enabled=False, last_modified__gt=timezone.now() - timedelta( days=7 ) )
-            logging.debug( 'Found % s disabled % s to clean.', objs.count(), key )
-            objs.delete()
+            val.objects_unfiltered.filter( enabled=False, last_modified__gt=timezone.now() - timedelta( days=7 ) ).delete()
 
 
 """
@@ -90,29 +122,29 @@ config = {
     'schedules': {
         'testing': {
             # Schedule
-            'snapshot - every':   timedelta( hours=1 ),
+            'snapshot-every':   timedelta( hours=1 ),
             # Creation
             'datasets':         ['dpool'], # TODO Select these from Web UI
-            'snapshot - prefix':  'auto - testing - ',
-            'snapshot - date':    ' % F_ % T',
+            'snapshot-prefix':  'auto-testing-',
+            'snapshot-date':    '%F_%T',
             # TODO Deletion code is not recursive friendly so this can currently NEVER be true
             'recursive':        False,
             # Removal
-            'max - age': timedelta( days=7 ),
-            'keep - at - most': 30,
+            'max-age': timedelta( days=7 ),
+            'keep-at-most': 30,
         },
        'testing2': {
             # Schedule
-            'snapshot - every':   timedelta( days=1 ),
+            'snapshot-every':   timedelta( days=1 ),
             # Creation
             'datasets':         ['dpool'],
-            'snapshot - prefix':  'auto - testing2 - ',
-            'snapshot - date':    ' % F_ % T',
+            'snapshot-prefix':  'auto-testing2-',
+            'snapshot-date':    '%F_%T',
             # TODO Deletion code is not recursive friendly so this can currently NEVER be true
             'recursive':        False,
             # Removal
-            'max - age': timedelta( days=7 ),
-            'keep - at - most': 30,
+            'max-age': timedelta( days=7 ),
+            'keep-at-most': 30,
         },
     },
 }
