373f2bf0be86e95c6c1f9cff890cb7c88ea78f72
Joshua Criss <jcriss@localhostsolutions.com>
2012-06-26 18:53:17 -0400

    Models for the network config app

--- solarsanweb/configure/models.py
+++ solarsanweb/configure/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
+import datetime
+
+
+from numarray import *
+
+from django.utils import timezone
+
+class iface(model.Models):
+    name = models.CharField('max_length=12')
+    ip = models.IPAddressField()
+    mtu = models.IntegerField()
+    netmask = models.IPAddressField()
+    proto = models.CharField('max_length=10')
+    type = models.CharField('max_length=12')
+    gateway = models.IPAddressField()
+
+class dns(models.Models):
+    search = models.IPAddressField()
+    server = []
+    iface = models.ForeignKey(iface)
+
 # Create your models here.
