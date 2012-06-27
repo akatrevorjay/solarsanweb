1ff1506703c87bb6bc4b0a893105e4b702b1aa11
Trevor Joynson (trevorj) <trevorj@localhostsolutions.com>
2012-06-26 23:47:10 -0400

    Use CeleryBeatScheduler in solarsan.utils, and rework network interface config model

--- solarsanweb/configure/models.py
+++ solarsanweb/configure/models.py
@@ -19,30 +19,27 @@ class Config( models.Model ):
 Cluster
 """
 
-class ClusterNode( models.Model ):
-    """ Recently seen peers """
-    ip = models.IPAddressField( unique=True )
-    #enabled = models.BooleanField( default=False )
-    last_seen = models.DateTimeField( auto_now=True )
-    #first_seen = models.DateTimeField()
-    #objects = EnabledModelManager()
-    #objects_all = models.Manager()
+#class ClusterNode( models.Model ):
+#    """ Recently seen peers """
+#    ip = models.IPAddressField( unique=True )
+#    #enabled = models.BooleanField( default=False )
+#    last_seen = models.DateTimeField( auto_now=True )
+#    #first_seen = models.DateTimeField()
+#    #objects = EnabledModelManager()
+#    #objects_all = models.Manager()
 
 """
 Network
 """
 
-class iface(model.Models):
-    name = models.CharField('max_length=12')
-    ip = models.IPAddressField()
-    mtu = models.IntegerField()
-    netmask = models.IPAddressField()
-    proto = models.CharField('max_length=10')
-    type = models.CharField('max_length=12')
+## TODO Fixtures for default network config
+class NetworkInterfaceConfig(model.Models):
+    name = models.CharField(max_length=32)
+    ipaddr = models.IPAddressField()
+    netmask = models.CharField()
     gateway = models.IPAddressField()
-
-class dns(models.Models):
-    search = models.IPAddressField()
-    server = []
-    iface = models.ForeignKey(iface)
-
+    proto = models.CharField(max_length=32)
+    #type = models.CharField(max_length=32)
+    mtu = models.IntegerField(null=True)
+    ## TODO Is JSONField best for DNS information? Easy I suppose..
+    dns = JSONField()
