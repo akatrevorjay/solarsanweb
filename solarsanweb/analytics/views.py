aff8fb33e9465c3f7e78b8d8ad55c3388c3f9a5d
Trevor Joynson (trevorj) <trevorj@localhostsolutions.com>
2012-06-28 00:40:36 -0400

    Make new analytics graphs based on d3 / nvd3 work nicely with all RRD datasources

--- solarsanweb/analytics/views.py
+++ solarsanweb/analytics/views.py
@@ -6,6 +6,7 @@ from django.views.decorators.csrf import csrf_exempt
 from django.views.decorators.cache import cache_page
 from django import http
 from django.views import generic
+from django.utils import timezone
 
 #from solarsan.utils import *
 from storage.models import Pool, Dataset, Filesystem, Snapshot, Pool_IOStat
@@ -18,7 +19,7 @@ from pyrrd.backend import bindings
 
 import logging
 # Get an instance of a logger
-logger = logging.getLogger(__name__)
+logger = logging.getLogger( __name__ )
 
 import json
 
@@ -29,13 +30,14 @@ import os, sys
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 
-def home(request, *args, **kwargs):
-    return render_to_response('analytics/home.html',
+def home( request, *args, **kwargs ):
+    return render_to_response( 'analytics/home.html',
         {'title': 'Analytics',
+         'graph': request.GET.get('name', 'dmu_tx'),
             },
-        context_instance=RequestContext(request))
+        context_instance=RequestContext( request ) )
 
-def render(request, *args, **kwargs):
+def render2( request, *args, **kwargs ):
     # TODO get programmatically
     #pool = Pool.objects.all()[0]
     #logger.error('args=%s kwargs=%s post=%s', args, kwargs, request.GET)
@@ -61,30 +63,63 @@ def render(request, *args, **kwargs):
     #    values['iops']['write'].append( (time, int(iostat.iops_write) ) )
     #    values['bandwidth']['read'].append( (time, int(iostat.bandwidth_read) ) )
     #    values['bandwidth']['write'].append( (time, int(iostat.bandwidth_write) ) )
-
+    values = {}
     for rrd_file in [request.GET['name']]:
-        rrd_path=os.path.join(settings.DATA_DIR, 'rrd', rrd_file + '.rrd')
-        rrd = RRD(rrd_path, mode="r")
-        rrd_data = rrd.fetch(resolution=int(request.GET['step']), cf='AVERAGE',
-                  start=int(request.GET['start']),
-                  end=int(request.GET['stop']),
-                  returnStyle='ds')
+        rrd_path = os.path.join( settings.DATA_DIR, 'rrd', rrd_file + '.rrd' )
+        rrd = RRD( rrd_path, mode="r" )
+        rrd_data = rrd.fetch( resolution=int( request.GET['step'] ), cf='AVERAGE',
+                  start=int( request.GET['start'] ),
+                  end=int( request.GET['stop'] ),
+                  returnStyle='ds' )
 
         values[rrd_file] = []
 
-        def fix_nan(x):
-            x = list(x)
+        def fix_nan( x ):
+            x = list( x )
             if x[1] != x[1]: x[1] = 0
             return [x[0] * 1000, x[1]]
 
         for ds in rrd_data.iterkeys():
-            values[rrd_file].append( { 'key': ds, 'values': map(fix_nan, rrd_data[ds]) } )
+            values[rrd_file].append( { 'key': ds, 'values': map( fix_nan, rrd_data[ds] ) } )
 
-    return http.HttpResponse(json.dumps(values), mimetype="application/json")
+    return http.HttpResponse( json.dumps( values ), mimetype="application/json" )
+
+def render( request, *args, **kwargs ):
+    rrd_file = request.GET['name']
+    start = int(request.GET['start'])
+    stop = 'stop' in request.GET and request.get['stop'] or timezone.now().strftime('%s')
+    step = int(request.GET['step'])
+
+    rrd_path = os.path.join( settings.DATA_DIR, 'rrd', rrd_file + '.rrd' )
+    rrd = RRD( rrd_path, mode="r" )
+    rrd_data = rrd.fetch( resolution=int( step ), cf='AVERAGE',
+              start=start,
+              end=stop,
+              returnStyle='ds' )
+
+    values = []
+
+#    def fix_nan( x ):
+#        x = list( x )
+#        if x[1] != x[1]: x[1] = 0
+#        return {'time': x[0] * 1000, 'value': x[1]}
+#
+#    for ds in rrd_data.iterkeys():
+#        values[rrd_file].append( { 'key': ds, 'values': map( fix_nan, rrd_data[ds] ) } )
+
+    def fix_nan( x ):
+        x = list( x )
+        if x[1] != x[1]: x[1] = 0
+        return [x[0] * 1000, int(x[1]) / 1024 / 1024]
+
+    for ds in rrd_data.iterkeys():
+        values.append( { 'key': ds, 'values': map( fix_nan, rrd_data[ds] ) } )
+
+    return http.HttpResponse( json.dumps( values ), mimetype="application/json" )
 
 
 #@cache_page(15)
-def graphs(request, *args, **kwargs):
+def graphs( request, *args, **kwargs ):
     pool = Pool.objects.all()[0]
     graphs = {}
 
@@ -92,7 +127,7 @@ def graphs(request, *args, **kwargs):
         graphs[iostat_graph] = pyflot.Flot()
 
     count = 50
-    iostats = pool.pool_iostat_set.order_by('timestamp')[:count]
+    iostats = pool.pool_iostat_set.order_by( 'timestamp' )[:count]
 
     #total = int(iostats[0].alloc + iostats[0].free)
     #graph['usage'] = {'values': [float(iostats[0].alloc / float(total) * 100), float(iostats[0].free / float(total) * 100)] }
@@ -101,18 +136,18 @@ def graphs(request, *args, **kwargs):
     bandwidth = {'read': [], 'write': []}
 
     for iostat in iostats:
-        time = int(iostat.timestamp.strftime('%s')) * 1000
+        time = int( iostat.timestamp.strftime( '%s' ) ) * 1000
 
-        iops['read'].append( (time, int(iostat.iops_read) ) )
-        iops['write'].append( (time, int(iostat.iops_write) ) )
-        bandwidth['read'].append( (time, int(iostat.bandwidth_read) ) )
-        bandwidth['write'].append( (time, int(iostat.bandwidth_write) ) )
+        iops['read'].append( ( time, int( iostat.iops_read ) ) )
+        iops['write'].append( ( time, int( iostat.iops_write ) ) )
+        bandwidth['read'].append( ( time, int( iostat.bandwidth_read ) ) )
+        bandwidth['write'].append( ( time, int( iostat.bandwidth_write ) ) )
 
-    graphs['iops'].add_series(iops['read'], label='IOPs Read', )
-    graphs['iops'].add_series(iops['write'], label='IOPs Write', )
+    graphs['iops'].add_series( iops['read'], label='IOPs Read', )
+    graphs['iops'].add_series( iops['write'], label='IOPs Write', )
 
-    graphs['bandwidth'].add_series(bandwidth['read'], label='BW Read', )
-    graphs['bandwidth'].add_series(bandwidth['write'], label='BW Write', )
+    graphs['bandwidth'].add_series( bandwidth['read'], label='BW Read', )
+    graphs['bandwidth'].add_series( bandwidth['write'], label='BW Write', )
 
     for iostat_graph in ['bandwidth', 'iops']: #usage
         graphs[iostat_graph]._options = {
@@ -163,31 +198,31 @@ def graphs(request, *args, **kwargs):
 
     for rrd_file in ['cache_result', 'arc_hitmiss', 'cache_size', 'dmu_tx']:
         #graph.add_line([ (x,y), (x,y) ])
-        rrd_path=os.path.join(settings.DATA_DIR, 'rrd', rrd_file + '.rrd')
-        rrd = RRD(rrd_path, mode="r")
+        rrd_path = os.path.join( settings.DATA_DIR, 'rrd', rrd_file + '.rrd' )
+        rrd = RRD( rrd_path, mode="r" )
 
         endTime = datetime.now()
-        startTime = endTime - timedelta(hours=5)
+        startTime = endTime - timedelta( hours=5 )
 
         graphs[rrd_file] = pyflot.Flot()
-        rrd = RRD(rrd_path, mode="r")
-        rrd_data = rrd.fetch(resolution=300 * 12, cf='AVERAGE',
-                  start=startTime.strftime('%s'),
-                  end=endTime.strftime('%s'),
-                  returnStyle='ds')
+        rrd = RRD( rrd_path, mode="r" )
+        rrd_data = rrd.fetch( resolution=300 * 12, cf='AVERAGE',
+                  start=startTime.strftime( '%s' ),
+                  end=endTime.strftime( '%s' ),
+                  returnStyle='ds' )
 
         for ds in rrd_data.keys():
             d = []
-            for (x,y) in rrd_data[ds]:
+            for ( x, y ) in rrd_data[ds]:
                 try:
-                    time = int(datetime.fromtimestamp(int(x)).strftime('%s')) * 1000
-                    data = int(y)
+                    time = int( datetime.fromtimestamp( int( x ) ).strftime( '%s' ) ) * 1000
+                    data = int( y )
                     if data > 1000:
                         data = data / 1000 / 1000
                 except:
                     data = 0
                 d.append( ( time, data ) )
-            graphs[rrd_file].add_series(d, label=ds, )
+            graphs[rrd_file].add_series( d, label=ds, )
 
         graphs[rrd_file]._options = {
             'xaxis': {  'mode': 'time',
@@ -233,8 +268,8 @@ def graphs(request, *args, **kwargs):
             },
         }
 
-    return render_to_response('analytics/graphs.html',
+    return render_to_response( 'analytics/graphs.html',
             {'graphs': graphs,
-                }, context_instance=RequestContext(request))
+                }, context_instance=RequestContext( request ) )
 
 
