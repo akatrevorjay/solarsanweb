aa5eebe67e8e25479a70d9eed661c6f69520a46c
Trevor Joynson (trevorj) <trevorj@localhostsolutions.com>
2012-07-25 14:13:13 -0400

    Switch context module for pools to use mongo

--- solarsanweb/solarsan/context_processors.py
+++ solarsanweb/solarsan/context_processors.py
@@ -1,9 +1,12 @@
 
-from storage.models import Pool
+from django_mongokit import get_database, connection
+from storage.models import zPool
 
 def pools(request):
     try:
-        pools = Pool.objects.all()
+        conn = get_database()[zPool.collection_name]
+        pools = conn.zPool.find()
+        #pools = Pool.objects.all()
         return {'pools': pools}
     except:
         return {}
