92a108e617950121793784bb24c990063e1b60cb
Trevor Joynson (trevorj) <trevorj@localhostsolutions.com>
2012-08-05 17:30:53 -0400

    Debug helper

--- solarsanweb/solarsan/utils.py
+++ solarsanweb/solarsan/utils.py
@@ -6,6 +6,7 @@ from django.utils import simplejson
 import string, os, sys
 import logging, datetime, time
 from django import http
+import inspect
 
 ##
 ## django-braces -- Nice reusable MixIns
@@ -13,6 +14,37 @@ from django import http
 ##
 from braces.views import LoginRequiredMixin, PermissionRequiredMixin, SuperuserRequiredMixin #, UserFormKwargsMixin, UserKwargModelFormMixIn
 from braces.views import SuccessURLRedirectListMixin, SetHeadlineMixin, CreateAndRedirectToEditView, SelectRelatedMixin
+
+"""
+Devel helpers
+"""
+
+class TrueDebug(object):
+    ### Alpha ###
+    """A simple debugger. Add debug() to a function and it prints the function name and any objects included. 
+    Adding True to locale prints the file name where the function is. Adding False to log turns the log off.
+    This feature can be modified to trace deeper and find the bugs faster, ending the puzzle box."""
+    def __init__(self, objects=None, locale=False, log=True, parents=False):
+        if log == False: return
+        current = inspect.currentframe()
+        if parents: self.get_parents(current)
+        self.true_debug(current, objects, locale)
+    def true_debug(self, current, objects, locale):
+        debug_string = 'Function: ' + str(inspect.getouterframes(current)[1][3])
+        #if locale == 'all': print inspect.getouterframes(current)[4]; return
+        if objects != None: debug_string += ' Objects: ' + str(objects)
+        if locale: debug_string += ' File: ' + str(inspect.getouterframes(current)[1][1])
+        print debug_string
+        return
+    def get_parents(self, current):
+        debug_string = 'Function: ' + str(inspect.getouterframes(current)[1][3]) + ' Parents:'
+        family = list(inspect.getouterframes(current))
+        for parent in family:
+            debug_string += ' ' + str(parent[4])
+        print debug_string
+        return
+#debug = TrueDebug
+
 
 """
 MixIns
