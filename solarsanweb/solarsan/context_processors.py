85f6fd0b7762dc1899d8beee327d2c07263a7c5b
Trevor Joynson (trevorj) <trevorj@localhostsolutions.com>
2012-08-05 17:30:46 -0400

    Raven context proc

--- solarsanweb/solarsan/context_processors.py
+++ solarsanweb/solarsan/context_processors.py
@@ -1,4 +1,5 @@
 
+from django.conf import settings
 from django_mongokit import get_database, connection
 from storage.models import zPool
 
@@ -10,4 +11,12 @@ def pools(request):
         return {'pools': pools}
     except:
         return {}
+
+
+def raven_dsn(request):
+    try:
+        return {'raven_dsn': settings.RAVEN_CONFIG['dsn']}
+    except:
+        return {}
+
 
