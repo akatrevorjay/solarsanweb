44ec713205668f6365c5576157b28056995936aa
Trevor Joynson (trevorj) <trevorj@localhostsolutions.com>
2012-09-14 01:58:12 -0400

    Piss on Komodo for now, I'll stick with vim, because it's not annoying.

--- manage.py
+++ manage.py
@@ -5,6 +5,14 @@ import os
 import sys
 execfile(os.path.join(os.path.dirname(__file__), 'solarsanweb', 'paths.py'))
 
+#from django.conf import settings
+#if settings.DEBUG:
+#    try:
+#        from dbgp.client import brkOnExcept
+#        brkOnExcept(host='localhost', port=9000)
+#    except:
+#        pass
+
 if __name__ == '__main__':
     from django.core.management import execute_from_command_line
     execute_from_command_line(sys.argv)
