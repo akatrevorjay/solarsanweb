3e2d4c90e4dd33a6cdb69834eb9eae086e69b04e
Trevor Joynson (trevorj) <trevorj@localhostsolutions.com>
2012-12-09 17:30:03 -0500

    Finally start using pyparsing, the first usage is for Pool.status()

--- solarsanweb/storage/pool.py
+++ solarsanweb/storage/pool.py
@@ -1,7 +1,7 @@
 
 #import logging
 import sh
-from collections import defaultdict
+from collections import defaultdict, OrderedDict
 #import re
 from solarsan.utils import LoggedException
 from datetime import datetime
@@ -12,6 +12,54 @@ from pypercube.expression import Sum, Min, Max, Median, Distinct
 
 import storage.base
 #import storage.device
+
+from pyparsing import *
+
+
+class _ZpoolStatusParser(object):
+    """Parses 'zpool status' output
+    # TODO status -v (verbose)
+
+    p = ZpoolStatusParser()
+    ret = p()
+    pp(ret)
+    """
+
+    p_section_content = Combine(restOfLine + ZeroOrMore(LineEnd()
+                                + Suppress(White(exact=8)) + restOfLine))
+    p_section = Suppress(':') + p_section_content
+
+    p_first_section = Word('pool') + p_section
+    p_not_first_section = NotAny(Word('pool')) + Word(alphas) + p_section
+
+    p_pool = Dict(Group(p_first_section)) + OneOrMore(Dict(Group(p_not_first_section)))
+    p_status = OneOrMore(Dict(Group(p_pool)))
+
+    def __call__(self, arg=None):
+        if not arg:
+            arg = sh.zpool('status').stdout
+        ret = OrderedDict()
+        for v in self.p_status.parseString(str(arg)).asList():
+            v = dict(v)
+            v['config'] = self._config_parser(v)
+            ret[v['pool']] = v
+        return ret
+
+    p_config_line = Dict(Group(Word(srange("[-:0-9A-z.]")).setResultsName('name')
+                         + Word(alphanums).setResultsName('state')
+                         + Word(alphanums).setResultsName('read')
+                         + Word(alphanums).setResultsName('write')
+                         + Word(alphanums).setResultsName('cksum')
+                         + Suppress(LineEnd())))
+    p_config = Suppress(restOfLine + LineEnd()) + OneOrMore(p_config_line)
+
+    def _config_parser(self, arg):
+        # Config parse for multi-pool (short)
+        # TODO handle children / whitespace in front
+        ret = OrderedDict()
+        for v in self.p_config.parseString(str(arg['config'])):
+            ret[v['name']] = v.asDict()
+        return ret
 
 
 class PoolAnalytics(CubeAnalytics):
@@ -242,41 +290,9 @@ class Pool(storage.base.Base):
         pool.status()
 
         """
-        ret = defaultdict(dict)
-        config = None
-        footer = None
-        skip = 1
-        for line in sh.zpool('status', self.name):
-            line = line.rstrip("\n")
-            if skip > 0:
-                skip -= 1
-                continue
-
-            if not config:
-                if line.startswith(' state:'):
-                    ret['state'] = line.strip().split(None, 1)[1]
-                elif line.startswith(' scan:'):
-                    ret['scan'] = line.strip().split(None, 1)[1]
-                elif line.startswith('config:'):
-                    skip = 2
-                    config = True
-
-            elif not footer:
-                line = line.strip("\t")
-                #cols = line.strip().split(None, 4)
-                #logging.debug('cols=%s', cols)
-
-                #config.update(dict(zip(['state', 'read', 'write', 'cksum'],
-                #                       cols[1:])))
-
-                if not line:
-                    footer = True
-
-            else:
-                if line.startswith('errors:'):
-                    ret['errors'] = line.split('errors: ', 1)[1]
-
-        return dict(ret)
+        p = _ZpoolStatusParser()
+        out = sh.zpool('status', self.name).stdout
+        return p(out)
 
     def iostat(self, capture_length=30):
         """Returns iostat of storage pool.
