d0ba10c0a1062db6fbfab9f79462c81b0b42f58f
Trevor Joynson (trevorj) <trevorj@localhostsolutions.com>
2012-12-09 18:55:57 -0500

    Forgot to add status.tasks and shutdown template

---
+++ solarsanweb/status/tasks.py
@@ -0,0 +1,35 @@
+
+import celery
+from celery import chain, group, task, chord, Task
+from celery.task import periodic_task, PeriodicTask
+from celery.schedules import crontab
+from celery.utils.log import get_task_logger
+logger = get_task_logger(__name__)
+
+from django.utils import timezone
+#from django.core.files.storage import Storage
+
+import os
+import time
+import re
+import sh
+from datetime import datetime, timedelta
+from django.utils import timezone
+from django.conf import settings
+
+
+"""
+Shutdown/Reboot
+"""
+
+
+@task
+def shutdown():
+    logger.warning("Shutting system down..")
+    return sh.shutdown('-h', 'now')
+
+
+@task
+def reboot():
+    logger.warning("Rebooting system..")
+    return sh.shutdown('-r', 'now')
