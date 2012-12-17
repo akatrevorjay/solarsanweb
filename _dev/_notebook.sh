2f1b43fc51d33e7857a953bcc8abbc159d845373
Trevor Joynson (trevorj) <trevorj@localhostsolutions.com>
2012-12-17 06:29:50 -0500

    Add in some old things

---
+++ _dev/_notebook.sh
@@ -0,0 +1,2 @@
+#!/bin/bash -e
+exec ipython notebook --ip='*' --ext=django_notebook
---
+++ _dev/_storage.py
@@ -0,0 +1,10 @@
+
+import storage.models as m
+import storage.cache as c
+import storage.tasks as t
+
+import storage.pool as spool
+import storage.dataset as sdataset
+import storage.device as sdevice
+import storage.target as starget
+
