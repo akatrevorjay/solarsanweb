7cd03080c97c2b7364f8f99b3afbaa748fce3df4
Trevor Joynson (trevorj) <trevorj@localhostsolutions.com>
2012-12-24 23:45:27 -0500

    Add nothings for scst backend

---
+++ solarsanweb/storage/target/backends/scst.py
@@ -0,0 +1,29 @@
+import logging
+
+
+class Target(object):
+    pass
+
+
+class SRPTarget(Target):
+    pass
+
+
+class ISCSITarget(Target):
+    pass
+
+
+class Device(object):
+    pass
+
+
+class Lun(object):
+    pass
+
+
+class Scst(object):
+    def write_config():
+        pass
+
+    def read_config():
+        pass
